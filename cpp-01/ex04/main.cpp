e90afbae7706f3e715b9fc7e1f10d3fb49e8df56
ethan <ethanlo0506@gmail.com>
2025-08-15 19:58:40 +0200

    avancer du --> cpp01->ex03

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -6,14 +6,24 @@ int main(int argc, char *argv[]){
     std::cerr << "error with value" << std::endl;
     return 1;
   }
+  std::string filename = argv[1];
+  size_t last_point = filename.find_last_of('.');
+  std::string second_file = filename.substr(0, last_point) + ".replace";
   std::ifstream file(argv[1]);
   if (!file.is_open()){
-    std::cerr << "error with files, can't open." << std::endl;
+    std::cerr << "error with name file, can't open." << std::endl;
+    return 1;
   }
+  std::ofstream file2(second_file.c_str());
+  if (!file2.is_open()){
+    std::cerr << "error with create file, can't open." << std::endl;
+    return 1;
+  } 
   std::string line;
   while (std::getline(file, line)){
-    std::cout << line << std::endl;
+    file2 << line << std::endl;
   }
   file.close();
+  file2.close();
   return 0;
 }
