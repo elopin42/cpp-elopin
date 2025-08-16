b5ded38c5bc0d3faea34426de88100356356b2c9
ethan <ethanlo0506@gmail.com>
2025-08-16 15:50:39 +0200

    cpp-02 -> ex00 -> done sans std::cout

--- cpp-02/ex00/Fixed.cpp
+++ cpp-02/ex00/Fixed.cpp
@@ -1,20 +1,20 @@
 #include "Fixed.hpp"
 
 mfci::mfci(){
-  std::cout << "constructeur by default call" << std::endl;
+  // std::cout << "constructeur by default call" << std::endl;
 }
 
 mfci::mfci(const mfci &other) {
-  std::cout << "constructeur call for copie" << std::endl;
+  // std::cout << "constructeur call for copie" << std::endl;
   (void) &other;
 }
 
 mfci &mfci::operator=(const mfci &other) {
-  std::cout << "operator for affectation" << std::endl;
+  // std::cout << "operator for affectation" << std::endl;
   (void) &other;
   return *this;
 }
 
 mfci::~mfci(){
-  std::cout << "pour supprimer" << std::endl;
+  // std::cout << "pour supprimer" << std::endl;
 }
