cdd42364658608e5e25d589d98dbab5bad6a0071
ethan <ethanlo0506@gmail.com>
2025-08-18 00:18:33 +0200

    au cazou

--- cpp-02/ex01/main.cpp
+++ cpp-02/ex01/main.cpp
@@ -1,8 +1,7 @@
 #include "Fixed.hpp"
 
 int main( void ) {
-Fixed
-a;
+Fixed a;
 Fixed const b( 10 );
 Fixed const c( 42.42f );
 Fixed const d( b );
@@ -11,6 +10,7 @@ std::cout << "a is " << a << std::endl;
 std::cout << "b is " << b << std::endl;
 std::cout << "c is " << c << std::endl;
 std::cout << "d is " << d << std::endl;
+std::cout << "a is " << a.toFloat() << " as float" << std::endl;
 std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 std::cout << "c is " << c.toInt() << " as integer" << std::endl;
