be1a946927f3e8e06eef58ef87ea355793452d75
ethan <ethanlo0506@gmail.com>
2025-08-19 16:47:55 +0200

    start cpp-02 -> ex02bis

--- cpp-02/ex02/main.cpp
+++ cpp-02/ex02/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-int
-main( void ) {
-Fixed
-Fixed const
-a;
-b( Fixed( 5.05f ) * Fixed( 2 ) );
+int main( void ) {
+Fixed a;
+Fixed const  b( Fixed( 5.05f ) * Fixed( 2 ) );
 std::cout << a << std::endl;
 std::cout << ++a << std::endl;
 std::cout << a << std::endl;
