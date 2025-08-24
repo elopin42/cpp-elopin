cc8ea79f58ab4ff273312e893d483bce02115d5e
elopin42 <ethanlo0506@gmail.com>
2025-08-25 01:38:12 +0200

    ok

---
+++ cpp-04/ex00/animal.cpp
@@ -0,0 +1,9 @@
+#include "animal.hpp"
+
+animal::animal();{
+  std::cout << "animal constructor called" << std::endl;
+}
+
+animal::~animal();{
+  std::cout << "animal destructeur called" << std::endl;
+}
---
+++ cpp-04/ex00/animal.hpp
@@ -0,0 +1,11 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+
+class animal {
+  protected:
+    std::string type;
+}
+
+#endif
