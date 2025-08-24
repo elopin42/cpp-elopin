70e29736bb91bb733c473de37df134e63860d964
elopin42 <ethanlo0506@gmail.com>
2025-08-24 23:07:57 +0200

    finish cpp-03

---
+++ cpp-03/ex02/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap{
+  protected:
+    std::string name;
+    int hit_points;
+    int energy_points;
+    int attack_damage;
+  public:
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    ClapTrap(const ClapTrap& other);
+    ClapTrap& operator=(const ClapTrap& other);
+    ClapTrap(const std::string& name);
+    ClapTrap();
+    ~ClapTrap();
+};
+
+#endif 
