492b620f592156237e9b3688ff0e2c06108ef28d
elopin42 <ethanlo0506@gmail.com>
2025-09-07 23:05:05 +0200

    start cpp05

---
+++ cpp05/Bureaucrat.cpp
@@ -0,0 +1,21 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : name("default"){
+  std::cout << "Bureaucrat constructor called" << std::endl;
+} 
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+  std::cout << "Bureaucrat copy assignment called for " << this->name << std::endl;
+  if (this != &other) {
+    Bureaucrat::operator=(other);
+  }
+  return *this;
+}
+
+Bureaucrat::Bureaucrat(const std::string& name) : name(name){
+  std::cout << "Bureaucrat constructor called for " << this->name << std::endl;
+} 
+
+Bureaucrat::~Bureaucrat() {
+  std::cout << "Bureaucrat destructor called" << std::endl;
+}
