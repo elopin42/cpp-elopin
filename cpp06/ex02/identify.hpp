039ee59fb847311ec01203feb04b855465e6bfca
elopin42 <ethanlo0506@gmail.com>
2025-09-21 20:46:08 +0200

    finish cpp6

---
+++ cpp06/ex02/identify.hpp
@@ -0,0 +1,23 @@
+#ifndef IDENTIFY_HPP
+# define IDENTIFY_HPP
+
+#include <string>
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>
+#include <typeinfo>
+
+class Base {
+  public:
+    virtual ~Base() {};
+};
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+Base* generate(void);
+void identify(Base* p);
+void identify(Base& p);
+
+#endif // !IDENTIFY_HPP
---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,12 @@
+#include "identify.hpp"
+
+int main() {
+    srand(time(0));
+
+    Base* obj = generate();
+    identify(obj);
+    identify(*obj);
+
+    delete obj;
+    return 0;
+}
