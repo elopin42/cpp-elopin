f027ab9728af9f033a7b5ea46d22490088932e1d
Ethan Lopin <elopin@f2r10s21.clusters.42paris.fr>
2025-09-23 17:40:09 +0200

    correction avec halem

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -2,16 +2,21 @@
 # define ITER_HPP
 
 #include <cstddef>
+#include <iostream>
 
 template <typename T, typename F>
-void iter(T *array, size_t length, F func) {
+void iter(T* array, size_t length, F &func) {
+    if (!array)
+        return ;
     for (size_t i = 0; i < length; i++) {
         func(array[i]);
     }
 }
 
 template <typename T, typename F>
-void iter(const T *array, const size_t length, const F func) {
+void iter(const T* array, const size_t length, const F &func) {
+    if (!array)
+        return ;
     for (size_t i = 0; i < length; i++) {
         func(array[i]);
     }
